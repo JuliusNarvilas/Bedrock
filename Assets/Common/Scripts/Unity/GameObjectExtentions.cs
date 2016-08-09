6f684b46301c08fdd531f2598cf5b7f5e4f5e622
Julius Narvilas <julius.narvilas@gmail.com>
2016-08-10 00:52:21 +0100

    Adding test

---
+++ Assets/Common/Scripts/Unity/GameObjectExtentions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Common.Unity
+{
+    public static class GameObjectExtentions
+    {
+        //public static bool IsPrefab(this GameObject i_GameObject)
+        //{
+        //    PrefabType test1 = PrefabUtility.GetPrefabType(prefab);
+        //    i_GameObject
+        //}
+    }
+}
---
+++ Assets/TestPrefab.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TestPrefab : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject popupContainer;
+    
+    private Animator anim;
+    private bool isClosing = false;
+
+    //*gameobject *animator
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        OnMigratedCallback();
+        Debug.Log("");
+    }
+
+    void OnMigratedCallback()
+    {
+        if (prefab != null)
+        {
+            //prefab
+            if (!prefab.scene.IsValid())
+            {
+                var prefabInstance = Instantiate(prefab);
+                if (popupContainer != null)
+                {
+                    prefabInstance.transform.SetParent(popupContainer.transform, false);
+                }
+            }
+            else
+            {
+                prefab.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(isClosing)
+        {
+            if(anim != null)
+            {
+                //anim.
+            }
+        }
+	}
+    
+}
